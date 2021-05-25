3848b2e40247d342b19ea321aba2e0b98a34498f
blawney <blawney@hsph.harvard.edu>
2021-05-25 15:28:02 -0400

    Fix issue with R renaming the cell barcodes

--- docker/sctk_dimreduce.R
+++ docker/sctk_dimreduce.R
@@ -6,12 +6,26 @@ RAW_COUNT_MATRIX <- args[1]
 PCA_DIM <- as.integer(args[2]) # how many PCA dimensions to use in the 'pre-reduction' prior to umap
 OUTPUT_UMAP_BASE <- args[3]
 
-# Import counts as a 
+# Import counts as a vanilla dataframe
 counts <- read.table(
     file = RAW_COUNT_MATRIX,
     sep = "\t",
     row.names = 1,
-    header=T
+    header=T,
+    check.names = FALSE
+)
+
+# above, we set check.names=F to prevent the mangling of the sample names.
+# Now, we stash those original sample names and run make.names, so that any downstream
+# functions, etc. don't run into trouble. In the end, we convert back to the original names
+orig_cols = colnames(counts)
+new_colnames = make.names(orig_cols)
+colnames(counts) = new_colnames
+
+colname_mapping = data.frame(
+    orig_names = orig_cols,
+    row.names=new_colnames,
+    stringsAsFactors=F
 )
 
 # Create an SCE object from the counts
@@ -39,7 +53,10 @@ sce <- getUMAP(
 
 # Export UMAP values to file
 df.umap <- SingleCellExperiment::reducedDim(sce, "UMAP")
+m = merge(df.umap,colname_mapping,by.x=0, by.y=0)
+rownames(m) = m[,'orig_names']
+m = m[,c('UMAP1','UMAP2')]
 
 # transpose for easier frontend handling
-df.umap <- t(df.umap)
-write.table(df.umap, OUTPUT_UMAP_BASE, sep='\t', quote=F)
+m <- t(m)
+write.table(m, OUTPUT_UMAP_BASE, sep='\t', quote=F)
